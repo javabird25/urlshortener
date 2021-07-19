2a0bb935d31769763c6e3d13ee6da72732f3409c
javabird25 <shockck84@gmail.com>
2021-07-19 12:39:52 +0500

    MySQL DB

--- backend/urlshortener/settings.py
+++ backend/urlshortener/settings.py
@@ -59,8 +59,12 @@ WSGI_APPLICATION = 'urlshortener.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'urlshortener',
+        'USER': 'urlshortener',
+        'PASSWORD': open('/run/secrets/db-password').read(),
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
