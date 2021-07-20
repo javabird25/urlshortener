faba73ed9562c76c677a13a233e4bd1619bef2e8
javabird25 <shockck84@gmail.com>
2021-07-20 16:18:23 +0500

    UrlTable test

--- frontend/src/components/__tests__/UrlTable.tsx
+++ frontend/src/components/__tests__/UrlTable.tsx
@@ -42,29 +42,30 @@ it.skip('changes the page on "Next page" button click', async () => {
         .mockResolvedValueOnce(
             {
                 data: {
-                    results: [
-                        {slug: '123', url: 'https://example.com'},
-                        {slug: '456', url: 'https://something.com'},
-                    ],
-                    count: 2,
+                    results: Array.from(Array(50).keys()).map(i => ({url: 'https://example.com', slug: (i + 1).toString()})),
+                    count: 60,
                     previous: null,
                     next: null,
                 }
             }
         )
+        .mockResolvedValueOnce(
+            {
+                data: {
+                    results: Array.from(Array(10).keys()).map(i => ({url: 'https://example.com', slug: (i + 51).toString()})),
+                    count: 60,
+                    previous: null,
+                    next: null,
+                }
+            }
+        );
 
     render(<UrlTable/>);
+    await waitFor(() => { screen.getByText('1'); });
+    screen.getByText('>').click();
 
     await waitFor(() => {
-        screen.getByText('Loading...');
-    });
-
-    await waitFor(() => {
-        screen.getByText('123');
-        screen.getByText('456');
-        screen.getByText('https://example.com');
-        screen.getByText('https://something.com');
-        expect(axiosMock.get).toBeCalledWith('/api/urls/?page=1');
+        screen.getByText('51');
     });
 });
 
